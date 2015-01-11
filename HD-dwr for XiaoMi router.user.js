0813ab82b7fdf52d2f4d972205c952deb5e6b981
ibeyond <ibeyond@gmail.com>
2015-01-11 23:53:27 +0800

    fix bugs

--- HD-dwr for XiaoMi router.user.js
+++ HD-dwr for XiaoMi router.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HD-dwr for XiaoMi router
 // @namespace    http://www.ibeyond.net/
-// @version      0.1
+// @version      0.2
 // @description  enter something useful
 // @author       You
 // @match        http://www.hd-dwr.com/*
@@ -9,6 +9,8 @@
 // @require      http://s.miwifi.com/d2r/js/base64.js
 // @require      http://code.jquery.com/jquery-2.1.3.min.js
 // ==/UserScript==
-var ed2k = GvodUrls.split("###")[GvodUrls.split("###").length-1]
-var xiaomi = '<a class=\'xiaomi\' target=\'_blank\' href=\''+ 'https://d.miwifi.com/d2r/?url='+Base64.encodeURI(ed2k)+'&src=ibeyond&name='+encodeURIComponent($('a.pic').children().attr('alt')) +'\'>' + $('a.pic').children().attr('alt') + '(小米下载）</a>';
-$(xiaomi).appendTo($('dd.playbtn'));
+var ed2k = GvodUrls.split("###");
+$.each(ed2k, function(i, u){
+    var xiaomi = '<a class=\'xiaomi\' target=\'_blank\' href=\''+ 'https://d.miwifi.com/d2r/?url='+Base64.encodeURI(u)+'&src=ibeyond&name='+encodeURIComponent($('a.pic').children().attr('alt')) +'\'>' + $('a.pic').children().attr('alt') + '&nbsp;' + (i + 1) + '&nbsp;(小米下载）</a>';
+	$(xiaomi).appendTo($('dd.playbtn'));
+});
