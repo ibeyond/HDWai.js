2209410d72e4c0027f33e72b9d8b41f88fb51d80
ibeyond <ibeyond@gmail.com>
2015-01-11 22:14:31 +0800

    移除磁力链

--- HDWai for XiaoMI.user.js
+++ HDWai for XiaoMI.user.js
@@ -1,14 +1,19 @@
 // ==UserScript==
 // @name         HDWai for XiaoMI 
 // @namespace    http://www.ibeyond.net/
-// @version      0.1
+// @version      0.2
 // @description  Download ed2k in XiaoMI Router
 // @author       iBeyond
 // @match        http://hdwai.com/*
 // @grant        none
 // ==/UserScript==
 $('a.btn-e').each(function(){
-  $(this).attr({'href':'https://d.miwifi.com/d2r/?url='+Base64.encodeURI($(this).attr('href'))+'&src=yyets&name='+encodeURIComponent($(this).parent().prev().prev().text()),'target':'_blank'});
+    $(this).attr({'href':'https://d.miwifi.com/d2r/?url='+Base64.encodeURI($(this).attr('href'))+'&src=yyets&name='+encodeURIComponent($(this).parent().prev().prev().text()),'target':'_blank'});
 });
-
+$('a.btn-m').each(function(){
+    $(this).remove();
+});
+$('a.xiaomi').each(function(){
+    $(this).remove();
+});
 
